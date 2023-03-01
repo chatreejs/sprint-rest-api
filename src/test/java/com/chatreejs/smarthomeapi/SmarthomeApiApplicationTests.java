b0171c101428615ed21dd3b6d392b843cdb8bb2a
Chatree.js <36321701+chatreejs@users.noreply.github.com>
2023-03-01 22:04:05 +0700

    chore: Init spring boot project

---
+++ src/test/java/com/chatreejs/smarthomeapi/SmarthomeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chatreejs.smarthomeapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmarthomeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
